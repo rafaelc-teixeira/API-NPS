c7d1077c21fce68bbea2ce5d0f8edbe51f9e82b9
rafaelRato <rafaelcteixeira1@hotmail.com>
2021-02-24 16:52:35 -0300

    Routes Get & Post

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get( "/", (req, res) => {
+  return res.json({usuario: "Rato", password: 582935, sexo: "Masculino", isDev: true});
+} );
+
+app.post( "/", (req, res) => {
+  return res.json({message: "Os dados foram salvos com sucesso!"});
+} );
+
 app.listen(3333, () => console.log("Server is Running!"));
