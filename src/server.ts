ace8fc68d42a5fc260d08191f67fd525b6fb27f5
rafaelRato <rafaelcteixeira1@hotmail.com>
2021-02-25 21:45:07 -0300

    TypeORM and SQLite initial configuration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
-
+import "./database";
 const app = express();
 
+
+
 app.get( "/", (req, res) => {
-  return res.json({usuario: "Rato", password: 582935, sexo: "Masculino", isDev: true});
+  return res.json({usuario: "RatoLino", password: 582935, sexo: "Masculino", isDev: true});
 } );
 
 app.post( "/", (req, res) => {
